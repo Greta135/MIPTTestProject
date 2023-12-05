b7b54660f2753c0ecf2582b3b136ec13835ddee5
Greta135 <garshina.rita@yandex.ru>
2023-12-05 12:17:44 +0300

    Добавьте файлы проекта.

---
+++ MIPTTestProject/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIPTTestProject
+{
+    internal class Calculator
+    {
+        public int Sum(int i, int j) // i + j 
+        {
+            int sum_i_j = i + j;
+            int sum = sum_i_j;
+            return sum; 
+        }
+        public int Diff(int i, int j) // i - j 
+        {
+            int diff_i_j = i - j;
+            int diff = diff_i_j;
+            return diff;
+        }
+        public int Multiply(int i, int j) // i * j 
+        {
+            int multipl_i_j = i * j;
+            int multipl = multipl_i_j;
+            return multipl;
+        }
+        public int Subtract(int i, int j) // i / j 
+        {
+            int sub_i_j = i / j;
+            int sub = sub_i_j;
+            return sub;
+        }
+    }
+}
---
+++ MIPTTestProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MIPTTestProject
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j = Convert.ToInt32(Console.ReadLine());
+            int sum = calculator.Sum(i , j);
+
+            Console.WriteLine("Our sum is");
+            Console.WriteLine(sum);
+            
+
+            int diff = calculator.Diff(i, j);
+
+            Console.WriteLine("Our difference is");
+            Console.WriteLine(diff);
+            
+
+            int multipl = calculator.Multiply(i, j);
+
+            Console.WriteLine("Our multiply is");
+            Console.WriteLine(multipl);
+
+            int sub = calculator.Subtract(i, j);
+
+            Console.WriteLine("Our subtract is");
+            Console.WriteLine(sub);
+            Console.ReadLine();
+
+        }
+    }
+}
