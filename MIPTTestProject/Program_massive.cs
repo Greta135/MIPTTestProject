ef9552903009b4216b0cd4a0c63def3f0773437d
Greta135 <40110131+Greta135@users.noreply.github.com>
2023-12-12 12:51:44 +0300

    Update Program_massive.cs

--- MIPTTestProject/Program_massive.cs
+++ MIPTTestProject/Program_massive.cs
@@ -6,29 +6,30 @@ namespace MIPTTestProject
     internal class Program_massive
 
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
+      {
+        int N;
+        Console.WriteLine("Введите число");
+        N = Convert.ToInt32(Console.ReadLine());
+        int[] x = new int[N];
+
+        for (int i = 0; i < x.Length; i++)
         {
-            int N;
-            Console.WriteLine("Введите число");
-            N = Convert.ToInt32(Console.ReadLine());
-            int[] x = new int[N];
-
-            for (int i = 0; i < N; i++)
-            {
-                Console.WriteLine("Введите число номер один");
-                x[i] = Convert.ToInt32(Console.ReadLine());
-              
-            }
-
-            Console.Write("\nИтоговый Массив:");
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write("{0:N} ", x[i]);
-            }
-
-            Console.Write("\n\nКонец просмотра массива");
-            Console.ReadKey();
-
+          int n = i + 1;
+          Console.WriteLine("Введите число номер " + n + ":");
+          x[i] = Convert.ToInt32(Console.ReadLine());
+        
         }
+        Console.Write("\n Итоговый Массив:");
+        for (int i = 0; i < N; i++)
+        {
+          Console.Write("{0:N} ", x[i]);
+        }
+
+        Console.Write("\n Конец просмотра массива");
+        Console.ReadKey();
+
+
+      }
     }
     }
