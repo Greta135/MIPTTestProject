7192f4c53fc9b49786997825b680c3f5a260d426
Greta135 <40110131+Greta135@users.noreply.github.com>
2023-12-12 11:36:56 +0300

    Add files via upload

---
+++ MIPTTestProject/Program_massive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace MIPTTestProject
+{
+    internal class Program_massive
+
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Введите число");
+            N = Convert.ToInt32(Console.ReadLine());
+            int[] x = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Введите число номер один");
+                x[i] = Convert.ToInt32(Console.ReadLine());
+              
+            }
+
+            Console.Write("\nИтоговый Массив:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("{0:N} ", x[i]);
+            }
+
+            Console.Write("\n\nКонец просмотра массива");
+            Console.ReadKey();
+
+        }
+    }
+    }
