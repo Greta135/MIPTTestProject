2815419f5678370737a9741a94b0df67d6e36b11
Greta135 <40110131+Greta135@users.noreply.github.com>
2023-12-18 23:34:50 +0300

    Update Program_massive.cs

--- MIPTTestProject/Program_massive.cs
+++ MIPTTestProject/Program_massive.cs
@@ -8,30 +8,43 @@ namespace MIPTTestProject
     {
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("Введите число");
-            string str = Console.ReadLine();
+
             int N;
-            bool result = int.TryParse(str, out N);
-            if (result == false) { return; }
-            int[] x = new int[N];
-
-            for (int i = 0; i < x.Length; i++)
+            bool resultfalse = true;
+            while (resultfalse)
             {
-                int n = i + 1;
-                Console.WriteLine("Введите число номер " + n + ":");
-                x[i] = Convert.ToInt32(Console.ReadLine());
+                string str = Console.ReadLine();
+                bool result = int.TryParse(str, out N);
+                if (result == true)
+                {
+                    int[] x = new int[N];
 
+                    for (int i = 0; i < x.Length; i++)
+                    {
+                        int n = i + 1;
+                        Console.WriteLine("Введите число номер " + n + ":");
+                        x[i] = Convert.ToInt32(Console.ReadLine());
+
+                    }
+                    Console.Write("\n Итоговый Массив:");
+                    for (int i = 0; i < N; i++)
+                    {
+                        Console.Write("{0:N} ", x[i]);
+                    }
+
+                    Console.Write("\n Конец просмотра массива");
+                    Console.ReadKey();
+                    resultfalse = false;
+                }
+                else
+                {
+                    Console.WriteLine("Введите число");
+                    bool resul = int.TryParse(str, out N);
+
+                }
             }
-            Console.Write("\n Итоговый Массив:");
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write("{0:N} ", x[i]);
-            }
-
-            Console.Write("\n Конец просмотра массива");
-            Console.ReadKey();
-
 
         }
     }
