052dfd5fb5ce56ca2a342ad5c1958972590a1cdf
Greta135 <40110131+Greta135@users.noreply.github.com>
2023-12-18 15:07:40 +0300

    Update Program_classes.cs

--- MIPTTestProject/Program_classes.cs
+++ MIPTTestProject/Program_classes.cs
@@ -1,1 +1,65 @@
-﻿
+using System;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace MIPTTestProject
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            PrivilegeMaster master = new PrivilegeMaster();
+            Customer customer = new Customer();
+            Seller seller = new Seller();
+
+            
+        }
+    }
+    class User
+    {
+        private string Autorization
+        { 
+            get 
+            {
+                return Autorization;
+            }
+            set
+            {
+                Autorization = value;
+            }
+        }
+        public string Order;
+        public void DeleteOrder(int id)
+        {
+            Console.WriteLine("delete order by id");
+        }
+        public void CreateOrder(string newValue)
+        {
+            Console.WriteLine("create new order");
+        }
+        public void SendOrder(string newValue)
+        {
+            Console.WriteLine("SendOrder is Successed");
+        }
+        
+    }
+    class Customer : User
+    {
+    }
+    class Admin : User
+    {
+    }
+    class Seller : User
+    {
+    }
+    class PrivilegeMaster
+    {
+        public string Privilege
+        {
+            get { return Privilege; }
+        }
+
+    }
+}﻿
